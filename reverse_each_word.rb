3a602031d5ec053ea36a94cd23a64a0d80adaec4
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-12-31 16:24:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
 def reverse_each_word(sentence1)
-  sentence1 = "Hi again, just making sure it's reversed!"
-  puts "sentence1".reverse
+  sentence1 = "Hello there, and who are you?"
+  puts "#{sentence1}".reverse
 end
