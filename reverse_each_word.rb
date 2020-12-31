b1e2f7ff031f12d175ed7edb413a724862c7f840
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-12-31 16:22:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,4 @@
-def reverse_each_word(string)
-  string = "Hi again, just making sure it's reversed!"
-  puts "string".reverse
-  
+def reverse_each_word(sentence1)
+  sentence1 = "Hi again, just making sure it's reversed!"
+  puts "sentence1".reverse
 end
-reverse_each_word(string)
